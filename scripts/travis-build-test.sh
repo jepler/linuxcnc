c070182f7e64b38ffa789e14ad149158d690289b
Jeff Epler <jepler@unpythonic.net>
2018-03-21 19:39:10 -0500

    let's try rtai in travis!  what could go wrong

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,6 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 cd src
-./autogen.sh
-./configure --with-realtime=uspace --disable-check-runtime-deps
-make -j2  # cores: ~2, bursted
-../scripts/rip-environment runtests
+case "${TRAVIS_TARGET-unspecified}" in
+uspace|unspecified)
+    ./autogen.sh
+    ./configure --with-realtime=uspace --disable-check-runtime-deps
+    make -j2  # cores: ~2, bursted
+    ../scripts/rip-environment runtests
+;;
+docs)
+    ./autogen.sh
+    ./configure --with-realtime=uspace --disable-check-runtime-deps --enable-build-documentation=yes
+    make -j2  docs # cores: ~2, bursted
+;;
+rtai)
+    ./autogen.sh
+    ./configure --with-realtime=/usr/realtime-2.6.32-122-rtai --disable-check-runtime-deps
+    make -j2  docs # cores: ~2, bursted
+esac
--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -1,4 +1,13 @@
-#!/bin/sh -e
+#!/bin/bash -e
+case "${TRAVIS_TARGET-unspecified}" in
+uspace|unspecified|docs) ;;
+rtai)
+    wget \
+        http://linuxcnc.org/dists/jessie/base/binary-amd64/linux-headers-3.16.0-9-rtai-amd64_3.16.7-6linuxcnc_amd64.deb \
+        http://linuxcnc.org/dists/jessie/base/binary-amd64/linux-image-3.16.0-9-rtai-amd64_3.16.7-6linuxcnc_amd64.deb \
+        http://linuxcnc.org/dists/jessie/base/binary-amd64/rtai-modules-3.16.0-9_5.0~test1.2015.12.06.23.g613a3e2_amd64.deb
+    sudo dpkg -i *.deb ;;
+esac
 sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
 sudo apt-get remove -f libreadline6-dev || true
