c00f4ff14934ab863383acf757a7b05013cfa601
Jeff Epler <jepler@unpythonic.net>
2020-04-17 21:56:55 -0500

    try to do more via docker

---
+++ scripts/travis-build-only.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+cd src
+./autogen.sh
+./configure --with-realtime=uspace --disable-check-runtime-deps --enable-build-documentation
+make -O -j$(nproc)
--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -1,9 +1,45 @@
 #!/bin/sh -e
-sudo sh -c 'echo deb-src http://us.archive.ubuntu.com/ubuntu/ xenial main universe >> /etc/apt/sources.list'
-grep . /etc/apt/sources.list /etc/apt/sources.list.d/*
+set -x
+cat /etc/os-release
+. /etc/os-release
+CODENAME=${UBUNTU_CODENAME-`echo "$VERSION" | grep -o '[a-z]*'`}
+echo "Distro is $ID-${CODENAME}"
+case "$ID-${CODENAME}" in
+    debian-wheezy)
+	sudo sh -c 'cat > /etc/apt/sources.list' <<- EOF
+	deb http://www.linuxcnc.org/ $CODENAME base
+	deb-src http://www.linuxcnc.org/ $CODENAME base
+	deb http://archive.debian.org/debian/ $CODENAME main contrib
+	deb-src http://archive.debian.org/debian/ $CODENAME main contrib
+	EOF
+    ;;
+    debian-*)
+	sudo sh -c 'cat >> /etc/apt/sources.list' <<- EOF
+	deb http://www.linuxcnc.org/ $CODENAME base
+	deb-src http://www.linuxcnc.org/ $CODENAME base
+	deb-src http://deb.debian.org/debian $CODENAME main contrib
+	EOF
+    ;;
+    ubuntu-*)
+	sudo sh -c "echo deb-src http://us.archive.ubuntu.com/ubuntu/ $CODENAME main universe >> /etc/apt/sources.list"
+    ;;
+esac
+
+grep . /etc/apt/sources.list /etc/apt/sources.list.d/* || true
 sudo apt-get update -qq
-sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
+sudo apt-get install -y lsb-release python devscripts equivs build-essential --no-install-recommends
 sudo apt-get remove -f libreadline6-dev || true
 sudo apt-get remove -f libreadline-dev || true
-debian/configure uspace noauto
-mk-build-deps -i -r -s sudo -t 'apt-get --no-install-recommends --no-install-suggests'
+case "${REALTIME-uspace}" in
+uspace)
+    debian/configure uspace noauto
+;;
+rtai)
+    debian/configure rtai
+;;
+esac
+grep . debian/control /dev/null
+mk-build-deps
+sudo dpkg -i linuxcnc-*.deb || true
+sudo apt-get -f install -y --no-install-recommends
+
